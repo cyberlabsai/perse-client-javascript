415fc77577f7365cac7d4e3dbc1d026d4b729fed
leoggo <38364010+leoggo@users.noreply.github.com>
2021-07-14 22:25:58 -0300

    Feature/no cache (#2)
    * removing example images
    
    * removing use of image token
    
    * removing the need for a cache and simplifying the functions
    
    * fixing readme
    
    Co-authored-by: leo <leonardo.gurgel@cyberlabs.com.br>

--- example/run_all.js
+++ example/run_all.js
@@ -2,31 +2,41 @@ const { createReadStream } = require("fs")
 const { FaceRecClient } = require("../index")
 require("dotenv").config()
 
-const main = async () => {
-    console.log("begin")
-
+const run_detect = async () => {
     const api_key = process.env.API_KEY
 
     const client = FaceRecClient(api_key)
 
-    const frame_1 = createReadStream("./images/musk1.jpeg")
-
-    const frame_2 = createReadStream("./images/musk2.jpeg")
+    const frame_1 = createReadStream("./images/img1.jpeg")
 
     console.log("detect")
     const faces = await client.detectFaces(frame_1)
 
     console.log(faces)
+}
 
-    const face_token = faces.image_token
+const run_compare = async () => {
+    const api_key = process.env.API_KEY
+
+    const client = FaceRecClient(api_key)
+
+    const frame_1 = createReadStream("./images/img1.jpeg")
+
+    const frame_2 = createReadStream("./images/img2.jpeg")
 
     console.log("compare")
 
-    const face_list = [face_token, frame_2]
-
-    const comp = await client.compareFaces(face_list)
+    const comp = await client.compareFaces(frame_1, frame_2)
 
     console.log(comp)
+}
+
+const main = async () => {
+    console.log("begin")
+
+    await run_detect()
+
+    await run_compare()
 
     console.log("end")
 }
